afe7309cb22d5e2d4ddef23f497e38d9c12f447b
Diego Nogueira <40200145@esmad.ipp.pt>
2023-03-04 15:39:39 +0000

    Dynamically changing date and time

--- scripts/controller/calcController.js
+++ scripts/controller/calcController.js
@@ -1,38 +1,67 @@
 class CalcController {
-     
-    constructor(){
-        
-        this._displayCalc = '0';
+
+    constructor() {
+
+        this._locale = 'pt-pt'
+        this._displayCalcEl = document.querySelector('#display'); // El - By covention to inform that it is an element
+        this._dateEl = document.querySelector('#date');
+        this._timeEl = document.querySelector('#time');
         this._currentDate;
         this.initialize();
-        
-    }
-
-    initialize(){
 
     }
 
-    get displayCalc(){
+    initialize() {
 
-        return this._displayCalc;
+        this.setDisplayDateTime();
+
+        // Function to run a time interval in milliseconds, to update the time. 
+        setInterval(() => {
+
+            this.setDisplayDateTime();
+
+        }, 1000);
+    }
+
+    /* When I create the setInterval() the time will be locked for a second, so that this doesn't happen I have to assign before the setInterval() the 
+    displayDate and DisplayTime variables, however to avoid repetition I chose to create a method and call only the method.*/
+
+    setDisplayDateTime() {
+
+        this.displayDate = this.currentDate.toLocaleDateString(this._locale);
+        this.displayTime = this.currentDate.toLocaleTimeString(this._locale);
 
     }
 
-    set displayCalc(value){
+    get displayCalc() {
+        return this._displayCalcEl.innerHTML;
+    }
 
+    set displayCalc(value) {
         this._displayCalc = value;
-
     }
 
-    get currentDate(){
-
-        return this._currentDate;
-
+    get displayDate() {
+        return this._dateEl.innerHTML;
     }
 
-    set currentDate(value){
+    set displayDate(value) {
+        return this._dateEl.innerHTML = value;
+    }
 
-        this._currentDate= value;
-        
+    get displayTime() {
+        return this._timeEl.innerHTML;
+    }
+
+    set displayTime(value) {
+        return this._timeEl.innerHTML = value;
+    }
+
+    get currentDate() {
+        return new Date();
+    }
+
+    set currentDate(value) {
+        this._currentDate = value;
     }
 }
